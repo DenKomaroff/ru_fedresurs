42b1d2abc9b10c54071d43bab2ae22d1516ac2ac
DenKomaroff <d@komsroff.me>
2026-05-27 22:54:17 +0300

    add base api to ism

---
+++ src/fedresurs/ism/api.py
@@ -0,0 +1,12 @@
+class BaseAPI:
+
+    def __init__(self, url):
+        pass
+
+    def _prepare_params(self, **kwargs):
+        params = None
+        if len(kwargs) > 0:
+            params = {}
+            for param in kwargs:
+                params.update({param.replace('_', '.'): kwargs.get(param)})
+        self.params = params
--- src/fedresurs/ism/messages.py
+++ src/fedresurs/ism/messages.py
@@ -1,14 +1,13 @@
-class Messages:
+from .api import BaseAPI
+
+class Messages(BaseAPI):
 
     def __init__(self, url):
+        super().__init__(url)
         self.url = f'{url}/v1/messages'
 
     def execute(self, ses, **kwargs):
-        params = None
-        if len(kwargs) > 0:
-            params = {}
-            for param in kwargs:
-                params.update({param.replace('_', '.'): kwargs.get(param)})
+        self._prepare_params(**kwargs)
         response = ses.request(method='get', url=self.url, params=params, verify=False)
         result = response.json() if response.status_code == 200 else response
         return result
